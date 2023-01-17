183dc80e07adda57dec53c8d62d2c9fef05018c4
gabe fabian <gabe.fabian@gmail.com>
2023-01-16 20:44:03 -0700

    ill have this conversation with you

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,24 @@ const HomePage = () => {
       </Head>
       <div class="grid grid-cols-3 grid-rows-3 place-items-center h-screen">
         <div class="col-start-2 row-start-2 ">
-          <div class="text-5xl text-slate-400 text-center">
-            Alex James Hardy
+          <div class="">
+            <p className="text-5xl text-slate-400 text-center">
+              Alex James Hardy
+            </p>
           </div>
-          <div class="text-5xl text-slate-300 text-center mt-10">
-            33.4372786, -111.4527465
+          <div>
+            <p className="text-5xl text-slate-300 text-center mt-10">
+              33.4372786, -111.4527465
+            </p>
+          </div>
+          <div>
+            <p className="text-xl text-slate-300 text-center mt-10">
+              "If love alone were enough to save you, you would have lived
+              forever."
+            </p>
           </div>
           <div class="text-xl text-slate-400 text-center mt-10">
-            "If love alone were enough to save you, you would have lived
-            forever."
+            4/5/91 - 1/17/19
           </div>
         </div>
       </div>
